2142a83c80c0489ae8e85e4d010759903e00c4c7
Bookwhale00 <Bookwhale00@gmail.com>
2023-04-26 14:37:58 +0900

    :card_file_box:Feat: User model 추가 #1 users.models.py users.admin.py 추가 admin.py에 필드 추가 아직 x

--- users/models.py
+++ users/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, name=None, gender=None, age=None, introduction=None):
+        if not email:
+            raise ValueError("회원가입 시 이메일이 필요합니다.")
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+            gender=gender,
+            age=age,
+            introduction=introduction,
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    email = models.EmailField(
+        verbose_name="email address",
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    name = models.CharField(max_length=50, null=False, blank=False)
+    GENDER_CHOICES = [
+        ('Male', '남성'),
+        ('Female', '여성'),
+    ]
+    gender = models.CharField(
+        max_length=6, 
+        choices=GENDER_CHOICES, 
+        null=False, 
+        blank=False)
+    age = models.IntegerField(null=True, blank=True)
+    introduction = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
+
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
