2fc23d93d285a33ea20b84b81d7885011c265ea6
Bookwhale00 <Bookwhale00@gmail.com>
2023-04-27 16:24:05 +0900

    :sparkles:Feat: 유저삭제기능 추가 #6 유저삭제 (회원탈퇴) 기능 추가

--- users/views.py
+++ users/views.py
@@ -4,19 +4,19 @@ from rest_framework.response import Response
 from users.serializers import UserSerializer, UserUpdateSerializer
 from users.models import User
 from rest_framework.generics import get_object_or_404
-# 세션 로그인, 로그아웃 모듈
+'''세션 로그인, 로그아웃 모듈'''
 from django.contrib.auth import authenticate, login, logout
 
 
 class UserView(APIView):
     def get (self, request, user_id):
-        # 사용자 정보 보기
+        '''사용자 정보 보기'''
         user = get_object_or_404(User, id=user_id)
         serializer = UserSerializer(user)
         return Response(serializer.data, status=status.HTTP_200_OK)
     
     def post(self, request):
-        # 회원가입
+        '''회원가입'''
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -24,7 +24,7 @@ class UserView(APIView):
         else:
             return Response({"message":f"${serializer.errors}"},status=status.HTTP_400_BAD_REQUEST)
     def put (self, request, user_id):
-        # 사용자 정보 수정
+        '''사용자 정보 수정'''
         user = get_object_or_404(User, id=user_id)
         if request.user == user:
             serializer = UserUpdateSerializer(user, data=request.data)
@@ -37,11 +37,16 @@ class UserView(APIView):
             return Response("권한이 없습니다.", status=status.HTTP_403_FORBIDDEN)
 
     def delete (self, request, user_id):
-        # 회원 탈퇴
-        pass
+        '''회원 탈퇴'''
+        user = get_object_or_404(User, id=user_id)
+        if request.user == user:
+            user.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            return Response("권한이 없습니다.", status=status.HTTP_403_FORBIDDEN)
 
 class LoginView(APIView):
-    # 유저 로그인
+    '''유저 로그인'''
     def post(self, request):
         user = authenticate(request, **request.data)
         if not user:
@@ -50,7 +55,7 @@ class LoginView(APIView):
         return Response({"message": "로그인 되었습니다."})
  
 class LogoutView(APIView):
-    # 유저 로그아웃
+    '''유저 로그아웃'''
     def post(self, request):
         logout(request)
         return Response({"message": "로그아웃 되었습니다."})
